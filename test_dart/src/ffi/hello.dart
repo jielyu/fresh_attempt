aa9f3a8f0b0ea3a432cc477a4f87665ea99d42c3
Jie Lyu <jiejielyu@outlook.com>
2021-08-06 17:22:14 +0800

    Add files via upload

---
+++ test_dart/src/ffi/hello.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:ffi' as ffi;
+import 'dart:io' show Platform, Directory;
+
+import 'package:path/path.dart' as path;
+
+// FFI signature of the hello_world C function
+typedef HelloWorldFunc = ffi.Void Function();
+// Dart type definition for calling the C foreign function
+typedef HelloWorld = void Function();
+
+void main() {
+  // Open the dynamic library
+  var libraryPath =
+      path.join(Directory.current.path, 'hello_library', 'libhello.so');
+  if (Platform.isMacOS) {
+    libraryPath =
+        path.join(Directory.current.path, 'hello_library', 'libhello.dylib');
+  }
+  if (Platform.isWindows) {
+    libraryPath = path.join(
+        Directory.current.path, 'hello_library', 'build/Release', 'hello.dll');
+  }
+
+  final dylib = ffi.DynamicLibrary.open(libraryPath);
+
+  // Look up the C function 'hello_world'
+  final HelloWorld hello = dylib
+      .lookup<ffi.NativeFunction<HelloWorldFunc>>('hello_world')
+      .asFunction();
+  // Call the function
+  hello();
+}
---
+++ test_dart/src/ffi/test_ffigen.dart
@@ -0,0 +1,24 @@
+import 'dart:io' show Platform, Directory;
+import 'dart:ffi' as ffi;
+import 'package:path/path.dart' as path;
+import 'hello_ffi.dart';
+
+int main() {
+    // Open the dynamic library
+  var libraryPath =
+      path.join(Directory.current.path, 'hello_library', 'libhello.so');
+  if (Platform.isMacOS) {
+    libraryPath =
+        path.join(Directory.current.path, 'hello_library', 'libhello.dylib');
+  }
+  if (Platform.isWindows) {
+    libraryPath = path.join(
+        Directory.current.path, 'hello_library', 'build/Release', 'hello.dll');
+  }
+
+  final dylib = ffi.DynamicLibrary.open(libraryPath);
+  
+  var nl = new NativeLibrary(dylib);
+  nl.hello_world();
+  return 0;
+}
