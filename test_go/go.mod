b5a1040c52495f9e8e19fd69c6227d99f8e4b8b3
wuyi <jiejielyu@outlook.com>
2021-11-16 11:22:58 +0800

    feat(test_go/goframe_test): add testcase for goframe

--- test_go/go.mod
+++ test_go/go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	fyne.io/fyne/v2 v2.1.1 // indirect
+	github.com/gogf/gf/v2 v2.0.0-beta
 	github.com/google/uuid v1.3.0 // indirect
 )
---
+++ test_go/goframe_test/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
+)
+
+func main() {
+	s := g.Server()
+	s.BindHandler("/", func(r *ghttp.Request) {
+		r.Response.Write("Hello World! GoFrame...")
+	})
+	s.Run()
+}
