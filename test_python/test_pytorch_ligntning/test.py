6a1a7a27ff69afd41224a5bf072a422db930618f
jielyu <jiejielyu@outlook.com>
2022-09-02 16:47:27 +0800

    feat(test_python/test_pytorch_ligntning/): 增加pytorch_lightning的测试示例

---
+++ test_python/test_pytorch_ligntning/test.py
@@ -0,0 +1,50 @@
+import os
+from torch import optim, nn, utils, Tensor
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
+import pytorch_lightning as pl
+
+
+# define the LightningModule
+class LitAutoEncoder(pl.LightningModule):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        # it is independent of forward
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = nn.functional.mse_loss(x_hat, x)
+        # Logging to TensorBoard by default
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+
+def main():
+    # define any number of nn.Modules (or use your current ones)
+    encoder = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
+    decoder = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
+
+    # init the autoencoder
+    autoencoder = LitAutoEncoder(encoder, decoder)
+
+    # setup data
+    dataset = MNIST(os.getcwd(), download=True, transform=ToTensor())
+    train_loader = utils.data.DataLoader(dataset)
+
+    # train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
+    trainer = pl.Trainer(limit_train_batches=100, max_epochs=1)
+    trainer.fit(model=autoencoder, train_dataloaders=train_loader)
+
+
+if __name__ == "__main__":
+    main()
