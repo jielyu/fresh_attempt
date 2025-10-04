827b26fb1578ca12db137e1bbf3c282147beb9fc
jielyu <jiejielyu@outlook.com>
2025-10-04 16:28:47 +0800

    feat:增加释放GIL的语句

--- test_pybind11/src/openmp.cpp
+++ test_pybind11/src/openmp.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <omp.h> // OpenMP 头文件
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
 
 void openmp_parallel()
 {
+    // 释放 GIL（关键！）
+    py::gil_scoped_release release;
+
 #pragma omp parallel for
     for (int i = 0; i < 8; ++i)
     {
