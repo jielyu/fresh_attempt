22c6f5b324f8ee37b6acfa0d20d71c76675736bf
jielyu <jiejielyu@outlook.com>
2025-10-05 20:55:15 +0800

    feat:调整部分测试代码的位置

--- test_pybind11/test/test.py
+++ test_pybind11/test/test.py
@@ -7,19 +7,8 @@ def test_multiply():
     print(f"multiply(6*7): {example.multiply(6, 7)}")
 
 
-def test_create_config():
-    print(f"create_config: {example.create_config()}")
-
-
-def test_print_json():
-    config = {"a": 1, "b": "hello"}
-    example.print_json(config)
-
-
 def main():
     test_multiply()
-    test_create_config()
-    test_print_json()
 
 
 if __name__ == "__main__":
